5d69912c08b1120e432ef7c11f90250b94be2581
devharis2 <harisiqbal.work@gmail.com>
2022-11-03 12:44:25 -0700

    Logo night

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,27 +2,32 @@ import Image from "next/image";
 import Link from "@components/ui/link";
 import cn from "classnames";
 import { siteSettings } from "@settings/site-settings";
+import LogoBlack from "../../../public/assets/images/logo-black.svg";
+import { useContext } from "react";
+import { colorsContext } from "@contexts/colors.context";
 
 const Logo: React.FC<React.AnchorHTMLAttributes<{}>> = ({
-	className,
-	...props
+  className,
+  ...props
 }) => {
-	return (
-		<Link
-			href={siteSettings.logo.href}
-			className={cn("inline-flex focus:outline-none", className)}
-			{...props}
-		>
-			<Image
-				src={siteSettings.logo.url}
-				alt={siteSettings.logo.alt}
-				height={siteSettings.logo.height}
-				width={siteSettings.logo.width}
-				layout="fixed"
-				loading="eager"
-			/>
-		</Link>
-	);
+  const { darkTheme } = useContext(colorsContext);
+
+  return (
+    <Link
+      href={siteSettings.logo.href}
+      className={cn("inline-flex focus:outline-none", className)}
+      {...props}
+    >
+      <Image
+        src={darkTheme ? siteSettings.logo.url : LogoBlack}
+        alt={siteSettings.logo.alt}
+        height={siteSettings.logo.height}
+        width={siteSettings.logo.width}
+        layout="fixed"
+        loading="eager"
+      />
+    </Link>
+  );
 };
 
 export default Logo;
