e10e061c1a7c41cbfd209fcefb657612252937ea
devharis2 <harisiqbal.work@gmail.com>
2022-11-23 09:01:15 -0800

    Changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,9 @@ import { i18n } from "next-i18next";
 import { getDirection } from "@utils/get-direction";
 
 export default class CustomDocument extends Document {
-
-	
+	static async getInitialProps(ctx: DocumentContext) {
+		return await Document.getInitialProps(ctx);
+	}
 	render() {
 		const { locale } = this.props.__NEXT_DATA__;
 		if (process.env.NODE_ENV !== "production") {
