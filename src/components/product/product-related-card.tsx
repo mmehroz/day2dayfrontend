8a6874d4fa00d7b29829c32ae6c3f66f65d5188e
devharis2 <harisiqbal.work@gmail.com>
2022-11-28 17:56:10 +0500

    Products Added

--- src/components/product/product-related-card.tsx
+++ src/components/product/product-related-card.tsx
@@ -62,7 +62,7 @@ const ProductRelatedCard: FC<ProductProps> = ({
       src?.toString()?.includes("repziocdn") ||
       src?.toString()?.includes("elementvape")
     ) {
-      return data?.details?.product_thumbnail;
+      return src;
     }
 
     return `${API_ENDPOINTS.NEXT_PUBLIC_REST_ENDPOINT}/public/assets/img/products/thumb/${src}`;
